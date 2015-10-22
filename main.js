a95c9f7d049e45d7901abfe3a33652105ddfbeea
joeltg <joelg@mit.edu>
2015-10-22 02:53:05 -0400

    wow!

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var scene, renderer, camera, rectangleMaterial, textMaterial;
 var container, stats;
 var controls;
 var objects = [], links = [], plane;
+var parens;
 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2(),
     offset = new THREE.Vector3(),
     INTERSECTED, SELECTED;
+
 
 function init() {
     scene = new THREE.Scene();
@@ -25,7 +27,7 @@ function init() {
 
     container = document.getElementById('content');
     container.appendChild( renderer.domElement );
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 60, (window.innerWidth) / window.innerHeight, 1, 10000 );
     controls = new THREE.TrackballControls( camera );
     controls.noZoom = false;
     controls.noPan = false;
@@ -67,13 +69,13 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function addForm(args, position, name, outputs) {
+function addForm(numArgs, position, name, outputs) {
     if (!outputs) outputs = ['out'];
     var argsScale = 20;
     var padding = 10;
     var textMesh = makeText(name, true);
     var nameWidth = textMesh.geometry.boundingBox.max.x - textMesh.geometry.boundingBox.min.x;
-    var argWidth = args.length * argsScale;
+    var argWidth = numArgs * argsScale;
     var height = 10;
     var extrudeMesh = makeFrame(Math.max(nameWidth, argWidth) + padding, height + padding);
     var form = compose([extrudeMesh, textMesh]);
@@ -81,17 +83,19 @@ function addForm(args, position, name, outputs) {
     var sgeometry = new THREE.SphereGeometry( 5 );
     var smaterial = new THREE.MeshPhongMaterial( {color: 0xffffff, shading: THREE.FlatShading} );
     var input;
-    for (var i = 0; i < args.length; i++) {
+    for (var i = 0; i < numArgs; i++) {
         input = new THREE.Mesh( sgeometry, smaterial );
         scene.add(input);
         THREE.SceneUtils.attach(input, scene, form);
-        input.position.set(20 * (i - (args.length / 2)), 10, 5);
+        input.position.set(20 * (i - (numArgs / 2)), 10, 5);
     }
 
-    var addInput = new THREE.Mesh(sgeometry, rectangleMaterial);
-    scene.add(addInput);
-    THREE.SceneUtils.attach(addInput, scene, form);
-    addInput.position.set(20 * (args.length / 2), 10, 5);
+    if (numArgs != -1) {
+        var addInput = new THREE.Mesh(sgeometry, rectangleMaterial);
+        scene.add(addInput);
+        THREE.SceneUtils.attach(addInput, scene, form);
+        addInput.position.set(20 * (numArgs / 2), 10, 5);
+    }
 
     for (var j = 0; j < outputs.length; j++) {
         var sphere = new THREE.Mesh(sgeometry, rectangleMaterial);
@@ -135,46 +139,21 @@ function distance(v1, v2) {
     return Math.sqrt(dx*dx+dy*dy+dz*dz);
 }
 
-function addLink(startNode, endNode) {
-    var start = startNode.position, end = endNode.position;
-    var dir = new THREE.Vector3().subVectors(end, start).normalize();
-    var origin = start;
-    var length = distance(end, start);
-    var hex = 0x000000;
-    var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
-    arrowHelper.startNode = startNode;
-    arrowHelper.endNode = endNode;
-    scene.add(arrowHelper);
-    links.push(arrowHelper);
-    return arrowHelper;
-}
-
 var getCurve = THREE.Curve.create(
-    function ( length ) { //custom curve constructor
-        //this.scale = (length === undefined) ? 1 : length;
-    },
-    function ( t ) { //getPoint: t is between 0-1
-        return new THREE.Vector3(0, t, 0); //.multiplyScalar(this.scale);
-    }
+    function ( ) { },
+    function ( t ) { return new THREE.Vector3(0, t, 0);  }
 );
 
-function addFancyLink(startNode, endNode) {
-    var start = new THREE.Vector3().addVectors(startNode.position, startNode.parent.position);
-    var end = new THREE.Vector3().addVectors(endNode.position, endNode.parent.position);
-    var dir = new THREE.Vector3().subVectors(end, start).normalize();
-    var length = distance(end, start);
+function addLink(startNode, endNode) {
 
     var path = new getCurve(length);
     var tubeGeometry = new THREE.TubeGeometry(path, 8, 2, 8, true);
     var link = new THREE.Mesh(tubeGeometry, rectangleMaterial);
-    link.position.set(start.x, start.y, start.z);
-    console.log(link.up);
-    link.rotation.z = link.up.angleTo(dir);
     link.startNode = startNode;
     link.endNode = endNode;
+    updateLink(link);
     scene.add(link);
     links.push(link);
-    console.log(link);
     return link;
 }
 
@@ -189,47 +168,36 @@ function compose(meshes) {
 }
 
 init();
+/*
+var cube1 = addForm(0, [30, 60, 0], "one");
+var cube2 = addForm(2, [0, 0, 0], "add");
+var cube3 = addForm(1, [-60, -90, 0], "print");
+var cube5 = addForm(0, [-60, 60, 0], "three");
 
-var cube = addForm([], [30, 60, 0], "one");
-var cube2 = addForm([0, 1], [0, 0, 0], "add");
-var cube3 = addForm([0], [-60, -90, 0], "print");
-var cube5 = addForm([], [-60, 60, 0], "three");
-
-var link = addFancyLink(getOutputPort(cube2), cube3.children[0]);
-var link2 = addFancyLink(getOutputPort(cube), cube2.children[1]);
-var link3 = addFancyLink(getOutputPort(cube5), cube2.children[0]);
-
+var link1 = addLink(getOutputPort(cube2), cube3.children[0]);
+var link2 = addLink(getOutputPort(cube1), cube2.children[1]);
+var link3 = addLink(getOutputPort(cube5), cube2.children[0]);
+*/
 function getOutputPort(form) {
     return form.children[form.children.length - 1];
 }
 
 function updateLinks() {
-    var direction, length, start, end;
 
     for (var i = 0; i < links.length ; i++) {
-        /*
-        start = links[i].startNode.position;
-        start = new THREE.Vector3().addVectors(links[i].startNode.position, links[i].startNode.parent.position);
-        end = new THREE.Vector3().addVectors(links[i].endNode.position, links[i].endNode.parent.position);
-
-        origin = start;
-        direction = new THREE.Vector3().subVectors(end, start).normalize();
-        length = distance(start, end);
-
-        links[i].position.set(origin.x, origin.y, origin.z);
-        links[i].setDirection(direction);
-        links[i].setLength(length);
-        */
-        start = new THREE.Vector3().addVectors(links[i].startNode.position, links[i].startNode.parent.position);
-        end = new THREE.Vector3().addVectors(links[i].endNode.position, links[i].endNode.parent.position);
-        direction = new THREE.Vector3().subVectors(end, start);
-        length = distance(start, end);
-        links[i].position.set(start.x, start.y, start.z);
-        links[i].rotation.z = 0;
-        //console.log(links[i].scale);
-        links[i].scale.set(1, length, 1);
-        links[i].rotation.z = ((start.x > end.x) ? 1 : -1) * links[i].up.angleTo(direction);
+        updateLink(links[i]);
     }
+}
+
+function updateLink(link) {
+    var start = new THREE.Vector3().addVectors(link.startNode.position, link.startNode.parent.position);
+    var end = new THREE.Vector3().addVectors(link.endNode.position, link.endNode.parent.position);
+    var direction = new THREE.Vector3().subVectors(end, start);
+    var length = distance(start, end);
+    link.position.set(start.x, start.y, start.z);
+    link.rotation.z = 0;
+    link.scale.set(1, length * 1.15, 1);
+    link.rotation.z = ((start.x > end.x) ? 1 : -1) * link.up.angleTo(direction);
 }
 
 function render() {
@@ -237,6 +205,119 @@ function render() {
     updateLinks();
     controls.update();
     renderer.render(scene, camera);
+}
+
+function parse(code) {
+    // (function arg1 arg2 (function2 etc etc) arg4)
+    var marker = "|";
+    while (code.indexOf(marker) >= 0)
+        marker += "|";
+    marker = ' ' + marker + ' ';
+    if (code.substring(0, 1) == '(' && code.substring(code.length - 1, code.length) == ')')
+        code = code.substring(1, code.length - 1);
+    else return code;
+    code = code.split('');
+    var parens = 0;
+    var quote = false;
+    for (var i = 0; i < code.length; i++) {
+        if (code[i] == '(') parens++;
+        else if (code[i] == ')') parens--;
+        else if (code[i] == '"') quote = !quote;
+        else if (code[i] == ' ' && parens == 0 && !quote)
+            code[i] = marker;
+    }
+    code = code.join('');
+    code = code.split(marker);
+    for (var j = 0; j < code.length; j++)
+        code[j] = parse(code[j]);
+    return code;
+}
+
+function sanitize(text) {
+    var t = text.replace(/\n(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, " ");
+    while (t != text) { t = text.replace(/\n(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, " "); }
+    t = text.replace(/\s\s(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, " ");
+    while (t != text) { text = t.replace(/\s\s(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, " "); }
+    t = text.replace(/\s\s(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, " ");
+    while (t != text) { text = text.replace(/\(\s(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, "("); }
+    t = text = text.replace(/\s\)(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, ")");
+    while (t != text) { text = text.replace(/\s\)(?=([^\"']*[\"'][^\"']*[\"'])*[^\"']*$)/, ")"); }
+    return text;
+}
+
+function load(text, splits) {
+    clear();
+    if (text.indexOf('(') < 0 && text.indexOf(')') < 0) text = '(' + text + ')';
+    var code;
+    for (var i = 0; i < splits.length; i++) {
+        code = text.substring(splits[i][0], splits[i][1]);
+        makeNode(code);
+    }
+}
+
+function clear() {
+    for (var i = 0; i < objects.length; i++) {
+        scene.remove(objects[i]);
+    }
+    for (var j = 0; j < links.length; j++) {
+        scene.remove(links[j]);
+    }
+    plane = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry( 1000, 1000, 8, 8 ),
+        new THREE.MeshBasicMaterial( { visible: false } )
+    );
+    scene.add( plane );
+    renderer.render(scene, camera);
+}
+
+function countParens(text) {
+    var c = 0;
+    var q = true
+    var splits = [];
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] == '"') q = !q;
+        else if (text[i] == '(' && q) {
+            c += 1;
+            if (c == 1) splits.push([i]);
+        }
+        else if (text[i] == ')' && q) {
+            c -= 1;
+            if (c == 0 && splits.length > 0) splits[splits.length - 1].push(i + 1);
+        }
+    }
+    return [c, splits];
+}
+
+editor.getSession().on('change', function(e) {
+    var text = editor.getValue();
+    var newParens = countParens(text);
+    console.log(newParens);
+    if (!parens) parens = [];
+    if (!(parens.toString() == newParens.toString())) {
+        parens = newParens;
+        if (parens[0] == 0 && text.length > 0) load(text.trim(), parens[1]);
+    };
+});
+
+function makeNode(args) {
+    console.log(args);
+    if (typeof args == "string") {args = parse(sanitize(args)); }
+
+    var node = addForm(args.length - 1, [0, 0, 0], args[0]);
+
+    var arg, link;
+    for (var i = 1; i < args.length; i++) {
+        arg = args[i];
+        if (typeof arg == "object" && arg.constructor === Array) {
+            arg = makeNode(arg);
+            link = addLink(getOutputPort(arg), node.children[i - 1]);
+        }
+        else {
+            arg = addForm(-1, [0, 0, 0], arg);
+            link = addLink(getOutputPort(arg), node.children[i - 1]);
+        }
+    }
+    return node;
 }
 
 function onDocumentMouseMove( event ) {
