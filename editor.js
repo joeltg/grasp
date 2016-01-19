28626e625e625a517ce66a69ca34f4bb2a6f6ef5
Joel Gustafson <joelg@mit.edu>
2016-01-18 21:28:42 -0500

    updated prompt text

--- editor.js
+++ editor.js
@@ -11,17 +11,19 @@ editor.setOptions({
     fontSize: 16
 });
 
-editor.focus();
-
 editor.setValue(';; Welcome to GRASP!' +
     '\n;; Go ahead and write Scheme code here,\n' +
     ';; and then hit \'Calculate\'.\n' +
     ';; Not every Scheme special form has cool\n' +
     ';; custom 3D visual syntax - right now\n' +
-    ';; it\'s only let, lambda, define, and\n' +
-    ';; set! - but everything should still\n' +
-    ';; render reasonably reliably!');
+    ';; it\'s only let[*/rec], lambda, define,\n' +
+    ';; and set! - but everything should\n' +
+    ';; still render reasonably reliably!');
+
+editor.clearSelection();
 
 editor.getSession().on('change', function(e) {
     // e.type, etc
 });
+
+editor.focus();
