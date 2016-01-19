e8c97006f674be429904b8f4b2148a3205ce2e08
Joel Gustafson <joelg@mit.edu>
2016-01-19 12:45:22 -0500

    added default code

--- editor.js
+++ editor.js
@@ -14,12 +14,19 @@ editor.setOptions({
 
 editor.setValue(';; Welcome to GRASP!' +
     '\n;; Go ahead and write Scheme code here,\n' +
-    ';; and then hit \'Calculate\'.\n' +
+    ';; and then hit \'Calculate\'.\n\n' +
     ';; Not every Scheme special form has cool\n' +
     ';; custom 3D visual syntax - right now\n' +
     ';; it\'s only let[*/rec], lambda, define,\n' +
     ';; and set! - but everything should\n' +
-    ';; still render reasonably reliably!');
+    ';; still render reasonably reliably!\n\n' +
+    ';; Click \'Calculate\' to render this code!\n' +
+    ';; Left-click and drag to move objects,\n' +
+    ';; and right-click and drag to rotate\n' +
+    ';; the view.\n\n' +
+    '(define (foo x y)\n' +
+    '    (+ x y))\n' +
+    '(foo 4 (+ 2 5))');
 
 editor.clearSelection();
 
